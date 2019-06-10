e3d01ef78fb765a7f8d74637021611e203cf1366
shubham570 <shubhammittal570@gmail.com>
2019-06-10 09:23:23 +0000

    This is the solution of first problem.

---
+++ problem1.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+
+print("Enter your name")
+choice=input()
+print("Enter your age"}
+choice1=input()
+
+print("This is the year in which you are 95 years old : ")
+import datetime
+c=datetime.datetime.now()
+d=c.year
+
+s= d - int(choice1) + 95
+print(s)
